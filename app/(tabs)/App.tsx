dac9434b4904ca8a7db0d29c5b6684d55b56d59c
Noohg Noej <odineyes@naver.com>
2025-04-17 21:48:19 +0000

    250418 Hello RN

---
+++ app/(tabs)/App.tsx
@@ -0,0 +1,22 @@
+import { View, Text, StyleSheet } from "react-native";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Hello React Native</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "white",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    fontSize: 28,
+    color: "black",
+  },
+});
